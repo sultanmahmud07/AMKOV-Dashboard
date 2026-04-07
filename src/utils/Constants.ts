80b2c7ee94298f5fea5218fb58b0a55744335953
sultanmahmud07 <asadkhan01862@gmail.com>
2026-04-07 14:48:31 +0600

    Added new live url with ssl

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,8 +1,7 @@
 //============= Live Url 
-const BASEURL = "http://localhost:9000/api/v1"   //live server connect
-// const BASEURL = "https://api.t-power.com.au/v1"   //live server connect
+const BASEURL = "https://api.devshimul.com/api/v1"   //live server connect
 export const BASEURL_SOCKET = "https://api.t-power.com.au"   // live socket url
-export const CLIENT_URL = "https://t-power.com.au"   // live Client url
+export const CLIENT_URL = "https://app.devshimul.com"   // live Client url
            
 
 export default BASEURL;
